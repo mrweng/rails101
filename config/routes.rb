47bfefe01775b1b3118542ea321813937acd206d
mrweng <mrweng1999@gmail.com>
2016-07-19 17:36:48 +0800

    Model generate post content:text group_id:integer rake db:migrate edit group.rb add "has_many :posts" edit post.rb add "belongs_to :group"
    Controller
    generate posts
    
    Routes.rb
    remove resource :groups
    add resources :groups do ~

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :groups
+  # xxx resources :groups
+  resources :groups do
+    resources :posts
+  end
+
   # You can have the root of your site routed with "root"
   root 'groups#index'
 
