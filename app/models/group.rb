0118c32ad7123d5f61992e0062e4d53a9afe972b
mrweng <mrweng1999@gmail.com>
2016-07-26 17:22:03 +0800

    generate model group_user group_id:integer user_id:integer
    model
    group
    user
    group_user

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,9 @@ class Group < ActiveRecord::Base
 
   has_many :posts
 
+  has_many :group_users
+  has_many :members, through: :group_users, sourve: :user
+
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   def editable_by?(user)
